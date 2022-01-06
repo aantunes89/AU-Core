69a65b48b94c015a0ed1570489a53d3ae8cb9bef
Andre <andrepalhares1989@gmail.com>
2022-01-05 22:22:28 -0300

    removing husky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,16 @@
-import { Component } from "@angular/core";
-import { COURSES } from "../db-data";
-import { Course } from "./model/course";
+import { Component } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Angular core deep dive"
+  title = 'Angular core deep dive';
 
   public courses = COURSES;
 
-  public onCourseSelected(course: Course): void {
-    console.log(course);
-    debugger;
-
-
-  }
+  public onCourseSelected(course: Course): void {}
 }
