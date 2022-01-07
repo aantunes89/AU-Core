f0503092d1469c582ee3a1028671c923a0868d9c
Andre <andrepalhares1989@gmail.com>
2022-01-07 17:36:41 -0300

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from "@angular/core";
-import { COURSES } from "../db-data";
-import { Course } from "./model/course";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
+import { COURSES } from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { Course } from './model/course';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = "Angular core deep dive";
+export class AppComponent implements AfterViewInit {
+  title = 'Angular core deep dive';
 
   public courses = COURSES;
+
+  @ViewChildren(CourseCardComponent, { read: ElementRef })
+  cards: QueryList<ElementRef>;
+
+  @ViewChild('courseImage')
+  image;
+
+  ngAfterViewInit(): void {
+    console.log(this.image);
+  }
 
   public onCourseSelected(course: Course): void {}
 }
