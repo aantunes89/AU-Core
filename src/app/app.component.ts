48a553879b366164ed3f9497e2ffaabe4691b2bf
Andre <andrepalhares1989@gmail.com>
2022-01-07 22:40:28 -0300

    terminando section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  QueryList,
-  ViewChild,
-  ViewChildren
-} from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { COURSES } from '../db-data';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
@@ -15,20 +8,16 @@ import { Course } from './model/course';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
+  public readonly headerImgSrc =
+    'https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png';
+
   title = 'Angular core deep dive';
 
   public courses = COURSES;
 
   @ViewChildren(CourseCardComponent, { read: ElementRef })
   cards: QueryList<ElementRef>;
-
-  @ViewChild('courseImage')
-  image;
-
-  ngAfterViewInit(): void {
-    console.log(this.image);
-  }
 
   public onCourseSelected(course: Course): void {}
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+  ViewChild
+} from '@angular/core';
+import { CourseImageComponent } from '../course-image/course-image.component';
 import { Course } from '../model/course';
 
 enum Category {
@@ -12,13 +25,28 @@ enum Category {
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements AfterViewInit, AfterContentInit {
   @Input() course: Course;
+
   @Input() cardIndex: number;
 
   @Output() courseEmmiter = new EventEmitter<Course>();
 
+  /* @ContentChild só consegue buscar os elementos projetados via ng-content
+    com o nome em questão (no caso "courseImage")
+   */
+  @ContentChild(CourseImageComponent, { read: ElementRef })
+  image: ElementRef;
+
+  @ContentChildren(CourseImageComponent, { read: ElementRef })
+  images: QueryList<CourseImageComponent | ElementRef>;
+
   constructor() {}
+  public ngAfterContentInit(): void {
+    console.log('LOG 1', this.images);
+  }
+
+  public ngAfterViewInit(): void {}
 
   public onCourseViewed(): void {
     this.courseEmmiter.emit(this.course);
